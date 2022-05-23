28409205508a86dec96f5f92eab7830c2063365e
kartavya <kartavyas@gmail.com>
2022-05-23 23:04:46 +1000

    connection and server set up

--- server.js
+++ server.js
@@ -1,3 +1,18 @@
 const express = require("express");
 const routes = require("./routes");
-const sequelize = require("./config/connectioin");
+const sequelize = require("./config/connection");
+
+//setting up express server
+const app =express();
+const PORT = process.env.PORT || 3001;
+
+//importing middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// turn on routes
+app.use(routes);
+
+//turn on connection to db and server
+sequelize.sync({ force:false }).then(() => { app.listen(PORT, ()=> console.log("NOW listening"));
+ });
