2b723dad16abb065cba963641f40dd5b0f658dfe
kartavya <kartavyas@gmail.com>
2022-05-24 22:22:50 +1000

    import routes in index.js

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,1 +1,10 @@
 const router = require("express").Router();
+const travellerRoutes = require("./travellerRoutes");
+const locationRoutes = require("./locationRoutes");
+const tripRoutes = require("./tripRoutes");
+
+router.use("/travellers", travellerRoutes);
+router.use("/locaations", locationRoutes);
+router.use("/trips", tripRoutes);
+
+module.exports = router;
